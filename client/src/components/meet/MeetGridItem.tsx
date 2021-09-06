fd865a6a4fb842e78b2fd59b0a52c9a4e19614c9
이도현 <ksmfou98@naver.com>
2021-09-06 21:00:33 +0900

    Fix : width height 값 스타일트컴포넌트로 변경

--- client/src/components/meet/MeetGridItem.tsx
+++ client/src/components/meet/MeetGridItem.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useRef, useState } from "react";
 import Styled from "styled-components";
 
-const VideoContainer = Styled.video`
+const VideoContainer = Styled.video<{ ItemWidth: number; ItemHeight: number }>`
     background-color: black;
     object-fit: cover; // video 태그 무조건 1:1 비율인걸 깨뜨려주는 얘임
+    width: ${(props) => props.ItemWidth}px;
+    height: ${(props) => props.ItemHeight}px;
 `;
 
 interface Props {
@@ -26,8 +28,8 @@ const MeetGridItem = ({ stream, muted, width, height }: Props) => {
     <VideoContainer
       ref={ref}
       muted={isMuted}
-      width={width}
-      height={height}
+      ItemWidth={width}
+      ItemHeight={height}
       autoPlay
     ></VideoContainer>
   );
