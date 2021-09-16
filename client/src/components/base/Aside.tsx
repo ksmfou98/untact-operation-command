da32ce683677326a516ead1aedf51470d4c74110
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-09-16 16:11:39 +0900

    Feat : Aside + Aside Title

--- client/src/components/base/Aside.tsx
+++ client/src/components/base/Aside.tsx
@@ -1,9 +1,19 @@
 import { palette } from "lib/styles/palette";
 import React from "react";
 import styled from "styled-components";
+import { BsPeopleCircle } from "react-icons/bs";
 
 const Aside = () => {
-  return <AsideBlock>asd</AsideBlock>;
+  return (
+    <AsideBlock>
+      <AsideTitle>
+        <div className="profile-img">
+          <BsPeopleCircle size="40" />
+        </div>
+        <div className="user-name">이도현</div>
+      </AsideTitle>
+    </AsideBlock>
+  );
 };
 
 const AsideBlock = styled.aside`
@@ -14,6 +24,20 @@ const AsideBlock = styled.aside`
   background-color: ${palette.mainColor};
   box-shadow: 3px 14px 15px 1px rgba(90, 90, 90, 0.15);
   z-index: 2;
+  padding: 40px 10px;
+  color: #fff;
+`;
+
+const AsideTitle = styled.div`
+  display: flex;
+  align-items: center;
+  .profile-img {
+    margin-right: 30px;
+  }
+  .user-name {
+    font-size: 20px;
+    font-weight: 400;
+  }
 `;
 
 export default Aside;
