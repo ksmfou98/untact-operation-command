2ff334b3556cf4f0848ae85bacb38aae0c60d594
이도현 <ksmfou98@naver.com>
2021-09-18 22:34:12 +0900

    Fix : 채널키 config 파일로 옮김

--- client/src/hooks/useChannelPluginEffect.ts
+++ client/src/hooks/useChannelPluginEffect.ts
@@ -1,10 +1,11 @@
 import ChannelService from "lib/ChannelService";
+import { CHANNEL_ID_PLUGIN_KEY } from "lib/config";
 import { useEffect } from "react";
 
 export default function useChannelPluginEffect() {
   useEffect(() => {
     ChannelService.boot({
-      pluginKey: "84990ec9-1a13-452b-aa08-7b799362940e", //please fill with your plugin key
+      pluginKey: CHANNEL_ID_PLUGIN_KEY, //please fill with your plugin key
       //   memberId: "유저ID",
       //   profile: {
       //     name: "유저Name",
