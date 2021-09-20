34561f2b823acb8e4aa77f71c9c81d71c2d5cb99
이지호 <cctv1998@naver.com>
2021-09-20 17:51:11 +0900

    Add : CalendarPage

---
+++ client/src/pages/CalendarPage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const CalendarPage = () => {
+  return (
+    <CalendarPageBlock>
+
+    </CalendarPageBlock>
+  );
+};
+
+const CalendarPageBlock = styled.div`
+  display: flex;
+  margin-top: 32px;
+`;
+export default CalendarPage;
