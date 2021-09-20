6ae4f5c94805dcff860b69d07c1076a3544ca9ee
이지호 <cctv1998@naver.com>
2021-09-20 19:46:26 +0900

    Style : CalendarPage

--- client/src/pages/CalendarPage.tsx
+++ client/src/pages/CalendarPage.tsx
@@ -1,16 +1,40 @@
 import React from "react";
 import styled from "styled-components";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
 const CalendarPage = () => {
   return (
     <CalendarPageBlock>
-
+      <CalendarMain>
+        <FullCalendar
+          initialView="dayGridMonth"
+          headerToolbar={{
+            left: "prev,next today",
+            center: "title",
+            right: "dayGridMonth,timeGridWeek,timeGridDay",
+          }}
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        />
+      </CalendarMain>
     </CalendarPageBlock>
   );
 };
 
 const CalendarPageBlock = styled.div`
-  display: flex;
-  margin-top: 32px;
+  width: 100%;
 `;
+
+const CalendarMain = styled.main`
+  .fc .fc-button-primary {
+    background-color: rgb(59, 87, 231);
+    border : none;
+  }
+  .fc .fc-button-primary:not(:disabled):active, .fc .fc-button-primary:not(:disabled).fc-button-active{
+    background-color: rgb(0, 73, 176);
+  }
+`;
+
 export default CalendarPage;
