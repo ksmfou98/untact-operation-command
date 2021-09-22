908d79c942afd2690d2684e05e3b1b8c143db118
이도현 <ksmfou98@naver.com>
2021-09-22 23:47:24 +0900

    Fix : cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -16,9 +16,19 @@ const server = http.createServer(app);
 app.use(
   cors({
     credentials: true,
-    origin: true,
+    origin: "*",
   })
 );
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
