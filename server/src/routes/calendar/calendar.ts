4f0efab4228b4ad0eb4146a6f4f14faba41dda0e
이지호 <cctv1998@naver.com>
2021-09-23 19:10:12 +0900

    Feat : updateSchedule

--- server/src/routes/calendar/calendar.ts
+++ server/src/routes/calendar/calendar.ts
@@ -7,7 +7,7 @@ export const createSchedule = async (req: Request, res: Response) => {
   const userId = res.locals.user._id;
   try {
     const schedules = await Calendar.find();
-    const schedule = new Calendar({ title, start, end, userId });
+    const schedule = new Calendar({ title, start, end, user: userId });
     await schedule.save();
 
     return res.status(201).json({
@@ -37,3 +37,35 @@ export const readSchedule = async (req: Request, res: Response) => {
 };
 
 //일정 수정
+export const updateSchedule = async (req: Request, res: Response) => {
+  const { title, start, end, scheduleId } = req.body;
+  const userId = res.locals.user._id;
+  try {
+    let schedule = await Calendar.findById({ _id: scheduleId });
+    if (!schedule) {
+      return res.status(400).json({
+        success: false,
+        message: "해당 일정이 존재하지 않습니다.",
+      });
+    }
+    if (schedule.user.toString() !== userId) {
+      return res.status(400).json({
+        success: false,
+        message: "일정 작성자가 아닙니다.",
+      });
+    }
+    schedule = await Calendar.findByIdAndUpdate(
+      { _id: scheduleId },
+      { title, scheduleId, start, end },
+      { new: true }
+    );
+    return res.status(200).json({
+      success: true,
+      schedule,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error,
+    });
+  }
+};
