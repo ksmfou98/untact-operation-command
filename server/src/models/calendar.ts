a38d64dd88e193a18e179f4aa0b6f6f7d179b574
이지호 <cctv1998@naver.com>
2021-09-23 17:02:17 +0900

    Fix : date => start , end 둘로 나눔

--- server/src/models/calendar.ts
+++ server/src/models/calendar.ts
@@ -2,8 +2,9 @@ import mongoose, { Document, Model, Schema } from "mongoose";
 
 export interface ICalendar {
   title: string;
-  date: Date;
-  user:string;
+  start: Date;
+  end: Date;
+  user: string;
 }
 export interface ICalendarMethod extends ICalendar, Document {}
 export interface ICalendarStatics extends Model<ICalendarMethod> {}
@@ -15,15 +16,20 @@ const CalendarSchema: Schema<ICalendarMethod> = new Schema(
       required: true,
       maxlength: 30,
     },
-    date: {
+    start: {
       type: Date,
       required: true,
       default: Date.now,
     },
-    user:{
+    end: {
+      type: Date,
+      required:false,
+      default: Date.now,
+    },
+    user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-    }
+    },
   },
   { timestamps: true }
 );
