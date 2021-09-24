ae535b0346df22af44993d3c0f48ad6918bc39b2
이지호 <cctv1998@naver.com>
2021-09-25 03:26:04 +0900

    Feat : recoil app에적용

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <RecoilRoot>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </RecoilRoot>,
   document.getElementById("root")
 );
 
