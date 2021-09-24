c3ea57c026a440a84d2a1dbc5294ab619de6a519
이지호 <cctv1998@naver.com>
2021-09-25 03:24:36 +0900

    Feat : useCalendarEffect 스케줄 불러오기

---
+++ client/src/hooks/calendar/useCalendarEffect.ts
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import { readScheduleLinstAPI } from "lib/api/claendar";
+
+export default function useCalendarEffect() {
+  const [schedules, setSchedules] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      const schedules = await readScheduleLinstAPI();
+      setSchedules(schedules);
+    };
+    getData();
+  }, []);
+  return { schedules };
+}
