8d96f6916baf4010c69ac4f19cec5a9b5ff76420
이지호 <cctv1998@naver.com>
2021-09-25 01:58:58 +0900

    Chore : recoil

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,12 +1,8 @@
 import mongoose from "mongoose";
 import "dotenv/config";
 
-const DBURL =
-  process.env.DBURL ||
-  "mongodb+srv://admin:test123@cafenual.3wvli.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
 mongoose
-  .connect(DBURL, {
+  .connect(process.env.DBURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
