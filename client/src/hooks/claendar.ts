9cf6ddf2da7c758fc7b85175f7424196cbbdca84
이지호 <cctv1998@naver.com>
2021-09-24 13:23:00 +0900

    Feat : calendarAPI crud

---
+++ client/src/hooks/claendar.ts
@@ -0,0 +1,47 @@
+import client from "./common/client";
+
+// 일정 목록 조회
+export const readScheduleLinstAPI = async () => {
+  const response = await client.get("/calendar/readSchedule/");
+  return response.data.schedules;
+};
+
+// 일정 추가
+export const createScheduleAPI = async (
+  title: string,
+  start: Date,
+  end?: Date
+) => {
+  const body = {
+    title,
+    start,
+    end,
+  };
+  const response = await client.post("/calendar/createSchedule/", body);
+  return response;
+};
+
+// 일정 삭제
+export const deleteScheduleAPI = async (scheduleId: string) => {
+  const response = await client.delete(
+    `/calendar/createSchedule/${scheduleId}`
+  );
+  return response.data.schedules;
+};
+
+// 일정 수정
+export const updateScheduleAPI = async (
+  title: string,
+  start: Date,
+  scheduleId: string,
+  end?: Date
+) => {
+  const body = {
+    title,
+    start,
+    scheduleId,
+    end,
+  };
+  const response = await client.patch("/calendar/updateSchedule/", body);
+  return response.data.schedules;
+};
