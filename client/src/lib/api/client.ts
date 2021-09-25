5d92306deeb6cd01307dc080d0027cc975f165fa
이도현 <ksmfou98@naver.com>
2021-09-25 15:09:19 +0900

    Fix : client.ts URL

--- client/src/lib/api/client.ts
+++ client/src/lib/api/client.ts
@@ -4,6 +4,6 @@ import { SERVER_URL } from "lib/config";
 const client = axios.create({
   withCredentials: true,
 });
-client.defaults.baseURL = `${SERVER_URL}api/v1`;
+client.defaults.baseURL = `${SERVER_URL}/api/v1`;
 
 export default client;
