a2df6989c8cd3d82cbbfb7c96ed6afd35d9addd5
이지호 <cctv1998@naver.com>
2021-09-26 13:00:50 +0900

    Feat : MembersPage

---
+++ client/src/pages/MembersPage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const MembersPage = () => {
+  return (
+    <MenberPageBlock>
+
+    <main></main>
+    </MenberPageBlock>
+  );
+};
+const MenberPageBlock = styled.div`
+  margin: 50px 0px 0px 25px;
+`;
+
+
+export default MembersPage;
