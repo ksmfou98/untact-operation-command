8a34b4ce37c6c401aa2ec6ea33590d130c36356d
이지호 <cctv1998@naver.com>
2021-09-26 15:08:04 +0900

    Feat : atoms claendarState

--- client/src/atoms/calendarState.ts
+++ client/src/atoms/calendarState.ts
@@ -1,11 +1,31 @@
 import { atom } from "recoil";
+import { IUserState } from "atoms/userState";
+import { now } from "moment";
+
+export interface IScheduleState {
+  _id: string;
+  title: string;
+  start: Date;
+  end: Date;
+  user: IUserState;
+}
+
+const initialState: IScheduleState = {
+  _id: "",
+  title: "",
+  start: new Date(),
+  end: new Date(),
+  user: {
+    _id: "",
+    email: "",
+    name: "",
+    role: "",
+    thumbnail: "",
+    friends: [],
+  },
+};
 
 export const scheduleState = atom({
   key: "calendarScheduleState",
-  default: {
-    title: "",
-    start: "",
-    end: "",
-    user: "",
-  },
+  default: initialState,
 });
