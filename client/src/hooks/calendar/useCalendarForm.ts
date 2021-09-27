0563ea4c1e43c2d6f1a46f14f5022216c1f626bf
이지호 <cctv1998@naver.com>
2021-09-28 02:07:15 +0900

    Feat : useCalendarForm-onChangeSchedule

--- client/src/hooks/calendar/useCalendarForm.ts
+++ client/src/hooks/calendar/useCalendarForm.ts
@@ -1,17 +1,23 @@
 import { scheduleState } from "atoms/calendarState";
+import { createScheduleAPI } from "lib/api/claendar";
 import { useRecoilState } from "recoil";
 
 export default function useCalendarForm() {
   const [schedule, setSchedule] = useRecoilState(scheduleState);
+  const { start, date, title, end } = schedule;
 
   const onChangeSchedule = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-
     setSchedule((prev) => ({ ...prev, [name]: value }));
   };
 
-  const onCreateSchedule = () => {
-    
+  const onCreateSchedule = async () => {
+    try {
+      await createScheduleAPI(title, date, start, end);
+    } catch (e) {
+      alert("게시물 작성에 실패했습니다");
+      console.log(e);
+    }
   };
 
   return {
