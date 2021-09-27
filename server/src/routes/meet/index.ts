6547d1ded302c505cb1e689b9c0ea02fc457f29c
이도현 <ksmfou98@naver.com>
2021-09-27 21:30:39 +0900

    Feat : create Meet API

--- server/src/routes/meet/index.ts
+++ server/src/routes/meet/index.ts
@@ -1,6 +1,10 @@
 import express from "express";
+import checkLoggedIn from "../../lib/checkLoggedIn";
+
 import * as meetCtrl from "./meet.ctrl";
 
 const router = express.Router();
+
+router.post("/create", checkLoggedIn, meetCtrl.createMeet);
 
 export default router;
--- server/src/routes/meet/meet.ctrl.ts
+++ server/src/routes/meet/meet.ctrl.ts
@@ -1,1 +1,19 @@
-export const createMeet = (req: Request, res: Response) => {};
+import { Request, Response } from "express";
+import Meet from "../../models/meet";
+
+export const createMeet = async (req: Request, res: Response) => {
+  try {
+    const meet = new Meet(req.body);
+    await meet.save();
+
+    return res.status(201).json({
+      success: true,
+      meet,
+    });
+  } catch (e) {
+    return res.status(500).json({
+      success: false,
+      e,
+    });
+  }
+};
