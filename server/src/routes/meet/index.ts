98ec0069ac345ca33d8626b6c1e0b48af8e10d5d
이도현 <ksmfou98@naver.com>
2021-09-28 19:51:06 +0900

    Feat : Meet Thumbnail Img Upload API

--- server/src/routes/meet/index.ts
+++ server/src/routes/meet/index.ts
@@ -5,7 +5,8 @@ import * as meetCtrl from "./meet.ctrl";
 
 const router = express.Router();
 
-router.post("/create", checkLoggedIn, meetCtrl.createMeet);
-router.get("/readMeetList", meetCtrl.readMeetList);
+router.post("/create", checkLoggedIn, meetCtrl.createMeet); // 미팅 생성
+router.post("/uploadMeetThumb", meetCtrl.uploadMeetThumb); // 미팅 썸네일 업로드
+router.get("/readMeetList", meetCtrl.readMeetList); // 미팅 목록 조회
 
 export default router;
--- server/src/routes/meet/meet.ctrl.ts
+++ server/src/routes/meet/meet.ctrl.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
+import multer from "multer";
+import fs from "fs";
 import Meet from "../../models/meet";
 
+// 미팅 생성
 export const createMeet = async (req: Request, res: Response) => {
   try {
     const meet = new Meet(req.body);
@@ -18,6 +21,37 @@ export const createMeet = async (req: Request, res: Response) => {
   }
 };
 
+// 미팅 썸네일 이미지 저장
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/meet_thumb/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage: storage }).single("meetThumb_img");
+
+export const uploadMeetThumb = async (req: Request, res: Response) => {
+  const dir = "./uploads/meet_thumb";
+  if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+
+  upload(req, res, (err) => {
+    // 프론트랑 연동하고 try catch로 변경
+    if (err) {
+      return res.json({ success: false, err });
+    }
+    return res.json({
+      success: true,
+      image: res.req.file.path,
+      fileName: res.req.file.filename,
+    });
+  });
+};
+
+// 미팅 목록 조회
 export const readMeetList = async (req: Request, res: Response) => {
   try {
     const meets = await Meet.find();
