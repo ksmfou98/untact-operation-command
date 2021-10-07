66aafdbbe43ce681657671a58329368a9a92cd49
이도현 <ksmfou98@naver.com>
2021-10-07 19:56:11 +0900

    Feat : 회의 비밀번호 체크 API

--- server/src/routes/meet/index.ts
+++ server/src/routes/meet/index.ts
@@ -9,5 +9,6 @@ router.post("/create", checkLoggedIn, meetCtrl.createMeet); // 회의 생성
 router.post("/uploadMeetThumb", meetCtrl.uploadMeetThumb); // 회의 썸네일 업로드
 router.get("/readMeetList", meetCtrl.readMeetList); // 회의 목록 조회
 router.get("/findMeet/:meetId", checkLoggedIn, meetCtrl.findMeet); // 회의 조회
+router.post("/checkPassword", checkLoggedIn, meetCtrl.meetPasswordCheck); // 회의 비밀번호 확인
 
 export default router;
