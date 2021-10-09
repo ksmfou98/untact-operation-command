63ad45e6ffe67c2b52c6ae976174364d00d48016
이도현 <ksmfou98@naver.com>
2021-10-09 20:35:04 +0900

    Feat : SearchForm UI

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,21 +4,21 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import recoilInitializer from "atoms/recoilInitializer";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+const queryClient = new QueryClient();
 
 // initializeState : 초기 상태 설정
-
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot initializeState={recoilInitializer}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
     </RecoilRoot>
   </React.StrictMode>,
 
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
