98cd8299c57119bfa840171c97d9b8b4ab37fa22
이지호 <cctv1998@naver.com>
2021-10-10 03:23:17 +0900

    Feat : settingPage route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,6 +11,7 @@ import RegisterPage from "pages/RegisterPage";
 import LoginPage from "pages/LoginPage";
 import MembersPage from "pages/MembersPage";
 import MeetPage from "pages/MeetPage";
+import SettimgPage from "pages/SettingPage";
 const App = () => {
   useChannelPluginEffect();
   return (
@@ -29,6 +30,7 @@ const App = () => {
             <Route path="/" exact component={MeetListPage} />
             <Route path="/schedule" exact component={CalendarPage} />
             <Route path="/members" exact component={MembersPage} />
+            <Route path="/setting" exact component={SettimgPage} />
           </MainLayout>
         </AsideLayout>
       </Switch>
