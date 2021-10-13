5c4c33e4a282de7bf372b33e99794d10a0f6ea94
이지호 <cctv1998@naver.com>
2021-10-14 04:27:28 +0900

    Fix : updateAPI password,name도 변경 가능하게 수정

--- client/src/pages/SettingPage.tsx
+++ client/src/pages/SettingPage.tsx
@@ -1,8 +1,3 @@
-import useSetting from "hooks/setting/useSetting";
-import { SERVER_URL } from "lib/config";
-import React from "react";
-import { AiOutlineCloudUpload } from "react-icons/ai";
-import NoIMG from "assets/image/notExistMeet.png";
 import SettingProfileImg from "components/setting/SettingProfileImg";
 
 const SettingPage = () => {
