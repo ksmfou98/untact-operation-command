dedf813cbfe9bd3d31d5b8eaf83ffcaea4a32cc6
이지호 <cctv1998@naver.com>
2021-10-18 17:23:52 +0900

    Add : 사진 downlod

--- client/src/assets/image/index.ts
+++ client/src/assets/image/index.ts
@@ -5,3 +5,4 @@ export { default as searchFail } from "./searchFail.png";
 export { default as noLogin } from "./noLogin.png";
 export { default as noData } from "./noData.png";
 export { default as colorLogo } from "./colorLogo.png";
+export { default as blackLogo } from "./blackLogo.png";
